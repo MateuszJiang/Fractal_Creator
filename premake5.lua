a7924084db2487d2afaac02babadff2a5ed5f8fa
Matthew Caine <mateuszczylok1997@gmail.com>
2021-01-09 12:14:48 +0000

    overall change meant to make the repository work with premake - will work on many different systems

---
+++ premake5.lua
@@ -0,0 +1,83 @@
+workspace "FractalCreator"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- include directories relative the root folder
+IncludeDir = {}
+
+project "FractalCreator"
+	location "FractalCreator"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/SFML-2.5.1/include",
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	libdirs
+	{
+		"%{prj.name}/vendor/SFML-2.5.1/lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "STATE_DEBUG"
+		runtime "Debug"
+		optimize "On"
+
+		links
+		{
+			"sfml-graphics-d.lib",
+			"sfml-system-d.lib",
+			"sfml-window-d.lib",
+			"sfml-audio-d.lib"
+		}
+
+	filter "configurations:Release"
+		defines "STATE_RELEASE"
+		runtime "Release"
+		optimize "On"
+
+		links
+		{
+			"sfml-graphics.lib",
+			"sfml-system.lib",
+			"sfml-window.lib",
+			"sfml-audio.lib"
+		}
+
+	filter "configurations:Dist"
+		defines "STATE_DIST"
+		runtime "Release"
+		optimize "On"
+
+		links
+		{
+			"sfml-graphics.lib",
+			"sfml-system.lib",
+			"sfml-window.lib",
+			"sfml-audio.lib"
+		}
